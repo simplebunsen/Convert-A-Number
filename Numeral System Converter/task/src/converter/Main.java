03a11567f3c5d5916b69e2531de0fb873ae0de56
simplebunsen <really@dontdoxme.com>
2020-04-27 15:56:03 +0200

    Stage 5: Can convert decimal numbers of any base <36.

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -3,74 +3,95 @@ package converter;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         int sourceRadix = scanner.nextInt();
         scanner.nextLine();
-        long num = 0;
-        if (sourceRadix == 1) {
-            long tempnum = scanner.nextInt();
-            for (int i = 0; i <= (int) (Math.log10(tempnum)); i++) {
-                num++;
-            }
-        } else {
-            num = Long.parseLong(scanner.nextLine(), sourceRadix);
-        }
-        int targetRadix = scanner.nextInt();
 
+        String[] numInputString = scanner.nextLine().split("\\.");
+
+        long integerPart = getIntPartAsDecimal(sourceRadix, numInputString[0]);
+        double fracPart = 0;
+        if (numInputString.length > 1){
+            fracPart = getFracPartAsDecimal(sourceRadix, numInputString[1]);
+        }
+
+        double numAsDecimal = integerPart + fracPart;
+
+        int targetRadix = scanner.nextInt();
         if(sourceRadix > 36 || targetRadix > 36){
             System.out.println("Radix too large");
             return;
         }
 
-
-        if (targetRadix == 1) {
-            while (num > 0) {
-                System.out.print(1);
-                num--;
-            }
-        } else {
-            System.out.println(Long.toString(num, targetRadix));
+        String result;
+        if(numInputString.length < 1 || targetRadix == 1){
+            result = getIntegerTarget(targetRadix, integerPart);
+        }else {
+            result = getIntegerTarget(targetRadix, integerPart) + getFracTarget(targetRadix, fracPart);
         }
-
+        System.out.println(result);
     }
 
-    private static void getOctalLastDigit() {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.println(n%8);
+    private static String getFracTarget(int targetRadix, double num) {
+        String output = ".";
+        double multiplied = num * targetRadix;
+
+        for (int i = 0; i < 5; i++) {
+            //Wtf am I doing here? Take the part INFRONT of the "." in multiplied (as String), parse it to
+            //a decimal Long, then I convert it toString in the specified Target radix, and append it to the back
+            //of my String after the fraction.
+            long intPartOfMultiplication = Long.parseLong(String.valueOf(multiplied).split("\\.")[0]);
+            output = output.concat(Long.toString(intPartOfMultiplication, targetRadix));
+
+
+            double fracPartOfMultiplication = Double.parseDouble("0." + String.valueOf(multiplied).split("\\.")[1]);
+
+            multiplied = fracPartOfMultiplication * targetRadix;
+        }
+        return output;
     }
 
-    private static void getBinary() {
-        long decimal = 1864;
-        long binary = 0;
+    private static String getIntegerTarget(int targetRadix, long num) {
+        switch (targetRadix){
+            case 1:
+                while (num > 0){
+                    System.out.print(1);
+                    num--;
+                }
+                return "";
+            default:
+                return Long.toString(num, targetRadix);
+        }
+    }
 
-        long currentPow = 0;
-        int currentPowOf;
-        for (int i = 0; true; i++) {
-            long prevPow = currentPow;
-            currentPow = (long) Math.pow(2, i);
-            currentPowOf = i;
-            if(currentPow > decimal) {
-                currentPow = prevPow;
-                currentPowOf--;
+    private static double getFracPartAsDecimal(int sourceRadix, String num) {
+        if(sourceRadix == 1) return 0;
+        double output = 0;
+        for (int i = 0; i < num.length(); i++) {
+            long currentNum = Long.parseLong(Character.toString(num.charAt(i)), sourceRadix);
+            //System.out.println("in fraction num: " + currentNum);
+            output += currentNum / Math.pow(sourceRadix, i+1);
+        }
+        return output;
+    }
+
+    private static long getIntPartAsDecimal(int sourceRadix, String num) {
+        long output = 0;
+        switch (sourceRadix){
+            case 1:
+                for (int i = 0; i <= (int) (Math.log10(Long.parseLong(num))); i++) {
+                    output++;
+                }
                 break;
-            }
+            default:
+                output = Long.parseLong(num, sourceRadix);
         }
-        //System.out.println("our largest pow value: " + currentPow);
-
-        long remainder = decimal;
-        //System.out.println("remainder at beginning: " + remainder);
-        while(remainder > 0){
-            if(remainder >= currentPow){
-                binary += (long) Math.pow(10, currentPowOf);
-                //System.out.printf("%d currently larger than %d, binary is now %d \n", remainder, currentPow, binary);
-                remainder -= currentPow;
-            }
-            currentPowOf--;
-            currentPow = (long) Math.pow(2, currentPowOf);
-            //System.out.printf("Remainder now: %d, currentPow now: %d \n", remainder, currentPow);
-        }
-        System.out.printf("%d is equal to 0b%d", decimal, binary);
+        return output;
     }
+
+
 }
